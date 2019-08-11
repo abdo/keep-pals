1d953b74a441d17952c73ac728b55f1b9cd63ba6
Abdo <abdopoetry@gmail.com>
2019-08-11 14:36:04 +0200

    Fix eslint

--- src/screens/FriendsScreen/index.js
+++ src/screens/FriendsScreen/index.js
@@ -1,8 +1,47 @@
-import { Text } from 'react-native';
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+} from 'native-base';
 import React from 'react';
 
 const FriendsScreen = () => {
-  return <Text>Friends</Text>;
+  return (
+    <Container>
+      <Content>
+        <List>
+          <ListItem thumbnail>
+            <Left>
+              <Thumbnail
+                source={{
+                  uri:
+                    'https://avatars0.githubusercontent.com/u/33037931?s=460&v=4',
+                }}
+              />
+            </Left>
+            <Body>
+              <Text>Sankhadeep</Text>
+              <Text note numberOfLines={1}>
+                Its time to build a difference . .
+              </Text>
+            </Body>
+            <Right>
+              <Button transparent>
+                <Text>View</Text>
+              </Button>
+            </Right>
+          </ListItem>
+        </List>
+      </Content>
+    </Container>
+  );
 };
 
 export default FriendsScreen;
